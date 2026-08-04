60b26a6088747ef85569975a1b2132d67b55f813
samiksha-create <samikshasahare2025@gmail.com>
2026-08-04 15:04:34 +0530

    Added Lab 4

--- MCA2504056_Samiksha_Sahare/Lab4/online-attendance-management/backend/admin.js
+++ MCA2504056_Samiksha_Sahare/Lab4/online-attendance-management/backend/admin.js
@@ -12,7 +12,7 @@ const adminSchema = new mongoose.Schema(
         },
         email: {
             type: String,
-            required: [true, 'Email is required'],
+            required: [true, 'Email is required '],
             unique: true,
             lowercase: true,
             trim: true,
