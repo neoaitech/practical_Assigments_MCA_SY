66a1c65370e6545deb17ba67621357992a3cd05b
Rohan Bhegade <rohanbhegade2004@gmail.com>
2026-08-06 14:52:46 +0530

    I Have Uploaded LAB6 Ass

---
+++ MCA2504136_Rohan_Bhegade/LAB6/app.py
@@ -0,0 +1,9 @@
+from calculator import *
+
+print("Calculator Application")
+
+print("Addition =",add(15,10))
+print("Subtraction =",subtract(15,10))
+print("Multiplication =",multiply(15,10))
+print("Division =",divide(15,10))
+
