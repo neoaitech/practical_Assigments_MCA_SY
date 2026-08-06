9dd57ecf73a0cd914859dce9fcc210cabb452aff
Ashwini Burgute <ashwiniburgute9@gmail.com>
2026-08-06 14:53:03 +0530

    Practical Lab Assignment 7

---
+++ MCA2504004_Ashwini_Burgute/Lab7/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(
+    host='redis-server',
+    port=6379,
+    decode_responses=True
+)
+
+@app.route('/')
+def hello():
+    count = r.incr('hits')
+    return f'Hello! This page has been viewed {count} times.\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
