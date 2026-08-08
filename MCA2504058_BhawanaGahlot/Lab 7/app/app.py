a756efb58fbe50f8d8f28bd628d43517f0545bb4
Bhawana Gahlot <gahlotbhawana9@gmail.com>
2026-08-08 11:22:04 +0530

    Added

---
+++ MCA2504058_BhawanaGahlot/Lab 7/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import redis
+
+app = Flask(___name___)
+r = redis.Redis(host='redis-server' , port=6379, decode_responses=True)
+
+@app.route('/')
+def hello():
+    count = r.incr('hits')
+    return f'Hello! This page has been viewed {count} times.\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
