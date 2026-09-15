5c96f3892006d9bde67901f851d61028e8e75aa2
RahulKP41 <prajapatirahul3180@gmail.com>
2026-09-15 11:16:27 +0530

    adding all lab tell 12

---
+++ MCA2504173_Rahul/Lab6/app.py
@@ -0,0 +1,8 @@
+from calculator import add, subtract, multiply, divide
+
+print("Calculator Application")
+
+print("Addition =", add(25, 10))
+print("Subtraction =", subtract(45, 11))
+print("Multiplication =", multiply(25, 14))
+print("Division =", divide(15, 10))
